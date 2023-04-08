4fe5225df5cda8aef1bdd72167fee59302aa4a69
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-04-07 23:36:51 -0300

    Automação de testes com selenium e Ada Tech Santander

---
+++ maven-archetype-archetype/src/main/java/test/archetype/App.java
@@ -0,0 +1,27 @@
+package test.archetype;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\driver\\chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        countTime(5000L);
+        driver.close();
+    }
+
+    private static void countTime(Long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ maven-archetype-archetype/src/test/java/test/archetype/AppTest.java
@@ -0,0 +1,27 @@
+package test.archetype;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\driver\\chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.close();
+    }
+}
